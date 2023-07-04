afcd5f2886d3427922d23f3a1af653f077d81181
Pentagon888 <gzegozmucin@gmail.com>
2023-07-04 21:33:20 +0300

    Initial commit

---
+++ projekto_failai/JS_script/Javascript.js
@@ -0,0 +1,17 @@
+let x=5;
+let y = 6;
+let z = x+y;
+console.log(z);
+
+x=50;
+y=10;
+
+let vardas = "Jonas";
+let vieta = "4";
+if (vieta === 1) {console.log( vardas, "užėmė 1 vietą");}
+else if (vieta === 2) {console.log( vardas, "užėmė 2 vietą");}
+else if (vieta === 3) {console.log( vardas, "užėmė 3 vietą");}
+else {console.log( vardas, "medalio nelaimejo");}
+
+for (let i=0; i<=16; i+=4)
+{console.log(i)}
---
+++ projekto_failai/JS_script/kodas.js
@@ -0,0 +1,20 @@
+let x=5;
+let y = 6;
+let z = x+y;
+console.log(z);
+
+let b = "Gzegoz Mucin";
+let c = " Mano vardas yra ";
+console.log(c+':'+b);
+let n = true;
+while (x<10){
+if (n){
+console.log('n=true');
+}else{
+console.log('n=false');
+}
+x++;
+n=!n; // n = false
+
+for (let i=0; i<=16; i+=4)
+{console.log(i)}
